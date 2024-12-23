6473902cc20dc5a611e6ab378d79af60658e5de6
Vaibhav Tandon <vasu.tan2017@gmail.com>
2024-12-23 15:57:12 +0530

    vercel

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ CORS(app)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')  # Fetch from .env
+# app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')  # Fetch from .env
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')  # Fetch from .env
 app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_USERNAME')  # Fetch from .env
 mail = Mail(app)
